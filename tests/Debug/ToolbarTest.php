ceffa4f17c22756e79b47d5d100318fa67d849ef
0xMatt <matthew.javelet@gmail.com>
2015-09-22 17:15:58 -0500

    100% coverage for reals this time

--- tests/Debug/ToolbarTest.php
+++ tests/Debug/ToolbarTest.php
@@ -7,7 +7,7 @@ use Fyuze\Http\Response;
 
 class ToolbarTest extends \PHPUnit_Framework_TestCase
 {
-    public function testToolbarRegisteresCollectors()
+    public function testToolbarRegisteresCollectorsAndRenders()
     {
         $toolbar = new Toolbar();
         $reflection = new \ReflectionClass($toolbar);
@@ -19,5 +19,6 @@ class ToolbarTest extends \PHPUnit_Framework_TestCase
         $toolbar->addCollector(new ResponseCollector(new Response()));
 
         $this->assertCount(1, $property->getValue($toolbar));
+        $this->assertTrue(strpos((string) $toolbar->render(), 'toolbar') !== false);
     }
 }
